9aaf319252c64f3284b09203284a7b5650d13c7f
Yannick Mauray <yannick.mauray@gmail.com>
2022-07-27 22:54:19 +0200

    Trying new stuff

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,20 @@
-import 'package:exif/exif.dart';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:photo_manager/photo_manager.dart';
+
+final dateFormatter = DateFormat('yy-MM-dd HH-mm-ss');
+final folderFormatter = DateFormat('yyyy/MM');
+
+class AssetEntityWrapper {
+  AssetEntityWrapper(this.entity);
+
+  final AssetEntity entity;
+  bool deleteFlag = false;
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,28 +23,21 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class PhotoMeta {
-  PhotoMeta({
-    required this.title,
-    required this.destination,
-    required this.createDateSecond,
-    required this.image,
-  });
-
-  final String title;
-  final String destination;
-  final int createDateSecond;
-  final Widget image;
-}
-
 class _HomePageState extends State<HomePage> {
-  final dateFormatter = DateFormat('yy-MM-dd HH-mm-ss');
-  final folderFormatter = DateFormat('yyyy/MM');
+  var page = 0;
+  var pageSize = 100;
+  var _pictures = <AssetEntity>[];
 
-  Future<List<PhotoMeta>> init() async {
+  @override
+  initState() {
+    super.initState();
+    _loadPhotos().then((pictures) => setState(() => _pictures = pictures));
+  }
+
+  Future<List<AssetEntity>> _loadPhotos() async {
     var permission = await PhotoManager.requestPermissionExtend();
+    var pictures = <AssetEntity>[];
     if (permission.isAuth) {
-      var output = <PhotoMeta>[];
       final List<AssetPathEntity> paths = await PhotoManager.getAssetPathList(
         onlyAll: true,
         filterOption: FilterOptionGroup(
@@ -42,57 +47,41 @@ class _HomePageState extends State<HomePage> {
               asc: false,
             ),
           ],
+          imageOption: const FilterOption(needTitle: true),
         ),
       );
 
-      final List<AssetEntity> pictures =
-          await paths.first.getAssetListPaged(page: 0, size: 100);
+      final rootPath = paths.first;
 
-      for (var picture in pictures) {
-        if (await picture.isLocallyAvailable()) {
-          var file = await picture.file;
-          var data = await readExifFromBytes(file!.readAsBytesSync());
-          debugPrint(data.toString());
-          var folder = folderFormatter.format(picture.createDateTime);
-          var filename =
-              "${dateFormatter.format(picture.createDateTime)} ${picture.id}";
-          var title = await picture.titleAsync;
-          var extension =
-              title.substring(1 + title.lastIndexOf(".")).toLowerCase();
-          var destination = '$folder/$filename.$extension';
-          output.add(
-            PhotoMeta(
-              title: title,
-              destination: destination,
-              createDateSecond: picture.createDateSecond ?? 0,
-              image: AssetEntityImage(
-                picture,
-                isOriginal: false, // Defaults to `true`.
-                thumbnailSize:
-                    const ThumbnailSize.square(200), // Preferred value.
-                thumbnailFormat: ThumbnailFormat.jpeg, // Defaults to `jpeg`.
-              ),
-            ),
-          );
+      var assets = await rootPath.getAssetListPaged(
+        page: page,
+        size: pageSize,
+      );
+
+      var wrappedAssets = assets
+          .where((e) => (e.title ?? '').toLowerCase().startsWith("img_"))
+          .map((asset) => AssetEntityWrapper(asset));
+
+      for (var wrappedAsset in wrappedAssets) {
+        var locallyAvailable = await wrappedAsset.entity.isLocallyAvailable();
+        if (!locallyAvailable || wrappedAsset.entity.title == null) {
+          wrappedAsset.deleteFlag = true;
         }
       }
 
-      //output.sort((a, b) {
-      //  if ((a.destination == '<folder>') && (b.destination == '<folder>')) {
-      //    return a.title.compareTo(b.title);
-      //  } else if (a.destination == '<folder>') {
-      //    return -1;
-      //  } else if (b.destination == '<folder>') {
-      //    return 1;
-      //  } else {
-      //    return a.createDateSecond - b.createDateSecond;
-      //  }
-      //});
-
-      return output;
-    } else {
-      return [];
+      pictures = wrappedAssets
+          .where((e) => !e.deleteFlag)
+          .map((e) => e.entity)
+          .toList();
+      //        //image: AssetEntityImage(
+      //        //  picture,
+      //        //  isOriginal: false, // Defaults to `true`.
+      //        //  thumbnailSize:
+      //        //      const ThumbnailSize.square(200), // Preferred value.
+      //        //  thumbnailFormat: ThumbnailFormat.jpeg, // Defaults to `jpeg`.
+      //        //),
     }
+    return pictures;
   }
 
   @override
@@ -101,26 +90,64 @@ class _HomePageState extends State<HomePage> {
       navigationBar: const CupertinoNavigationBar(
         middle: Text('PhotoSync'),
       ),
-      child: FutureBuilder<List<PhotoMeta>>(
-        future: init(),
-        builder: (context, snapshot) {
-          return snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData
-              ? Material(
-                  child: CupertinoScrollbar(
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) => ListTile(
-                        title: Text(snapshot.data![index].title),
-                        subtitle: Text(snapshot.data![index].destination),
-                        leading: snapshot.data![index].image,
-                      ),
-                    ),
-                  ),
-                )
-              : Container();
-        },
+      child: Material(
+        child: CupertinoScrollbar(
+          child: ListView.builder(
+            itemCount: _pictures.length,
+            itemBuilder: (context, index) =>
+                _PhotoCard(picture: _pictures[index]),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _PhotoCard extends StatefulWidget {
+  const _PhotoCard({
+    Key? key,
+    required AssetEntity picture,
+  })  : _picture = picture,
+        super(key: key);
+
+  final AssetEntity _picture;
+
+  @override
+  State<_PhotoCard> createState() => _PhotoCardState();
+}
+
+class _PhotoCardState extends State<_PhotoCard> {
+  var destination = '';
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    var title = widget._picture.title!;
+    var folder = folderFormatter.format(widget._picture.createDateTime);
+    var id = widget._picture.id;
+    if (Platform.isIOS) {
+      id = title.substring(title.lastIndexOf('_') + 1, title.lastIndexOf('.'));
+    }
+    var filename =
+        "${dateFormatter.format(widget._picture.createDateTime)} $id";
+    var extension = title.substring(1 + title.lastIndexOf(".")).toLowerCase();
+    destination = '$folder/$filename.$extension';
+
+    Future.delayed(Duration(seconds: Random.secure().nextInt(5)), () {
+      setState(() {
+        _isLoading = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget._picture.title!),
+      subtitle: Text(destination),
+      //leading: snapshot.data![index].image,
+      trailing: _isLoading ? const CircularProgressIndicator() : null,
     );
   }
 }
